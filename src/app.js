e88e9d37c0bba0a3fb00f8e34c8a86ebb9e40c78
Tomoyuki Nohara <peace098beat.t@gmail.com>
2021-10-11 01:19:22 +0900

    Add src

---
+++ src/app.js
@@ -0,0 +1,187 @@
+// アプリケーション作成用のモジュールを読み込み
+const {app, BrowserWindow} = require('electron');
+
+
+function gCreateWindow(html_file){
+    win = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+      },
+      // width: 800, height: 600,
+      // fullscreen: true,
+      // alwaysOnTop: true
+    });
+
+    win.loadFile(html_file);
+    // win.webContents.openDevTools();
+    
+    return win;
+  }
+
+
+class TimerApp {
+  constructor(work_sec, calm_sec) {
+    this.mode = 0; // init
+    this.counter = work_sec; // init
+    this.intervals = [work_sec, calm_sec];
+
+    this.timer1 = null;
+    this.win0 = null;
+    this.win1 = null;
+    this.wins = [null, null];
+    
+    this.win0 = this.getWindow(0)
+    this.win1 = this.getWindow(1)
+
+    this.win0.show();
+    this.win1.hide();
+  }
+
+  getWindow(win_id){
+    console.log(this.timer1);
+
+    if(win_id == 0){
+      let win = this.win0;
+      if(win===null){
+        win = gCreateWindow("work.html");
+        this.win0 = win;
+        this.win0.on('closed', () => {
+          this.win0 = null;
+        });
+
+        var timerTime = this.counter;
+        win.webContents.on('did-finish-load', () => {
+            win.webContents.send('timer-change',  timerTime);
+        });
+
+      }
+
+      return win;
+
+    }else if(win_id==1){
+
+      let win = this.win1;
+
+      if(win===null){
+        win = gCreateWindow("calm.html");
+
+        this.win1 = win;
+        this.win1.on('closed', () => {
+          this.win1 = null;
+        });
+      }
+
+      var timerTime = this.counter;
+      win.webContents.on('did-finish-load', () => {
+            win.webContents.send('timer-change',  timerTime);
+        });
+      return win;
+
+    }
+  }
+
+  showWindow(win_id){
+
+    console.log('showWindow');
+    console.log(win_id);
+
+    win = this.getWindow(win_id);
+
+    console.log(win);
+
+    win.show();
+    win.moveTop();
+  }
+
+
+  hideWindow(win_id){
+    console.log('hideWindow');
+
+    console.log(win_id);
+
+    win = this.getWindow(win_id);
+
+    console.log(win);
+    win.hide();
+  }
+
+  showCurrentWindow(){
+    this.showWindow(this.mode);
+  }
+
+  main(){
+
+    let mode = this.mode;
+
+    this.counter -= 1;
+
+    // calm 時は常に表示
+    if(mode == 1){
+      this.showWindow(1);
+    }
+
+    if(this.counter == 0){
+
+      if(mode == 0){
+        this.hideWindow(0);
+        this.showWindow(1);
+        this.mode = 1;
+      }else if(mode == 1){
+        this.hideWindow(1);
+        this.showWindow(0);
+        this.mode = 0;
+      }
+
+      this.counter = this.intervals[this.mode];
+    }
+  }
+
+  stop(){
+    clearInterval(this.timer1);
+  }
+
+  start(){
+    this.timer1 = setInterval(()=>{
+      this.main();
+    }, 1000);
+  }
+
+}
+
+// ************************************************************
+//
+// sequences
+//
+// ************************************************************
+
+let timer_app;
+
+function startTimerApp(){
+  timer_app = new TimerApp(25*60, 5*60);
+  // timer_app = new TimerApp(5, 16); // [sec]
+  timer_app.start();
+}
+
+// ************************************************************
+//
+// sequences
+//
+// ************************************************************
+
+//  初期化が完了した時の処理
+app.on('ready', startTimerApp);
+
+// 全てのウィンドウが閉じたときの処理
+app.on('window-all-closed', () => {
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+app.on('activate', () => {
+  timer_app.showCurrentWindow();
+});
+
